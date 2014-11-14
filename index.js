590884a5fae7e6ed94ce1c2064a5541a13632b1f
Aaron Storck <aaronstorck@gmail.com>
2014-11-14 05:29:42 -0500

    Updated the emblem used in the node packages index.js

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
 /*
-               Super.JS - Super Extendable API Framework
 
-              .<@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@>.
-            .<@@@@@@   $$$$$$$$$$$$$$$$$$$$$\^^^^^^/$$$$@@@>.
-         .<@@@@@<   .$$$$$'~       '~'$$$$$$$\  /$$$$$$>@@@@@>.
-      .<@@@@@<'   o$$$$$$                `'$$$$$$$$$$$$  '>@@@@@>.
-   .<@@@@@<'    o$$$$$$oo.                  )$$$$$$$$$$     '>@@@@@>.
-   '<@@@@@<    o$$$$$$$$$$$.                                 >@@@@@>'
-     '<@@@@<  o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$oooooo...    >@@@@>'
-       '@@@@< $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$)>@@@@>'
-         '<@@@@$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$@@@@>'
-           '<@@@@$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$@@@@>'
-             '<@@@@<    .oooo.                    .$$@@@@>'
-               '<@@@@oo$$$$$$$o..             ..o$$@@@@>'
-                 '<@@@@$$$$$$$$$$$$$oooooooo$$$$$@@@@>'
-                   '<@@@@'$$$$$$$$$$$$$$$$$$$$$@@@@>'
-                     '<@@@@<   ~"SSSSSS"~   >@@@@>'
-                       '<@@@@<            >@@@@>'
-                         '<@@@@<        >@@@@>'
-                           '<@@@@<    >@@@@>'
-                             '<@@@@<>@@@@>'
-                               '<@@@@@@>'
-                                 '<@@>'
-                                   ^^
 
-*/
+  sSSs   .S       S.    .S_sSSs      sSSs   .S_sSSs        .S    sSSs
+ d%%SP  .SS       SS.  .SS~YS%%b    d%%SP  .SS~YS%%b      .SS   d%%SP
+d%S'    S%S       S%S  S%S   `S%b  d%S'    S%S   `S%b     S%S  d%S'
+S%|     S%S       S%S  S%S    S%S  S%S     S%S    S%S     S%S  S%|
+S&S     S&S       S&S  S%S    d*S  S&S     S%S    d*S     S&S  S&S
+Y&Ss    S&S       S&S  S&S   .S*S  S&S_Ss  S&S   .S*S     S&S  Y&Ss
+`S&&S   S&S       S&S  S&S_sdSSS   S&S~SP  S&S_sdSSS      S&S  `S&&S
+  `S*S  S&S       S&S  S&S~YSSY    S&S     S&S~YSY%b      S&S    `S*S
+   l*S  S*b       d*S  S*S         S*b     S*S   `S%b     d*S     l*S
+  .S*P  S*S.     .S*S  S*S         S*S.    S*S    S%S    .S*S    .S*P
+sSS*S    SSSbs_sdSSS   S*S          SSSbs  S*S    S&S  sdSSS   sSS*S
+YSS'      YSSP~YSSY    S*S           YSSP  S*S    SSS  YSSY    YSS'
+                       SP                  SP
+                       Y                   Y
+
+ */
 
 module.exports.Class = require('./core/base.js');
 module.exports.Application = require('./core/application.js');
