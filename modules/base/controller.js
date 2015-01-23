69147fd1e77690493a6e99aafce26bddb1367bc5
Aaron Storck <aaronstorck@gmail.com>
2015-01-22 21:27:50 -0500

    Updated the base controller to setup and execute model validations.

--- modules/base/controller.js
+++ modules/base/controller.js
@@ -35,6 +35,7 @@ module.exports = SuperJS.Class.extend({
   //verify the request by transforming, validating, and sanitizing parameters
   verifyRequest: function(req) {
 
+
     //maintain reference to the currrent instance
     var self = this;
 
@@ -48,6 +49,7 @@ module.exports = SuperJS.Class.extend({
       var transforms = [];
       var validations = [];
       var sanitizations = [];
+      var modelValidations = [];
 
       //loop through all the parameters for this action and
       //append transforms, validations, and sanitizations to their respective lists
@@ -56,15 +58,39 @@ module.exports = SuperJS.Class.extend({
         //store the parameters value
         parameters[param] = req.param(param);
 
+        //set value to default if its not passed
+        if( !parameters[param] ) {
+          parameters[param] = self.blueprint.actions[req.action].params[param].default;
+        }
+
         //setup transforms
         transforms = transforms.concat(self.app.services.transform.setup(self.blueprint.actions[req.action].params[param].transform, parameters, param));
 
         //setup validations
-        validations = validations.concat(self.app.services.validate.setup(self.blueprint.actions[req.action].params[param].validate, param, parameters[param]));
+        validations = validations.concat(self.app.services.validate.setup(self.blueprint.actions[req.action].params[param].validate, param, parameters[param], 'parameter'));
 
         //setup sanitizations
         //sanitizations = sanitizations.concat(self.app.services.sanitize.setup(self.blueprint.actions[req.action].params[param].sanitize, parameters, param));
 
+        if( self.blueprint.actions[req.action].params[param].model ) {
+
+          //if the parameter is a model but the value is null/undefined, default parameter to empty object
+          if( Object.keys(self.blueprint.actions[req.action].params[param].model.validate).length > 0 && !parameters[param]) {
+            parameters[param] = {};
+          }
+
+          for( var attribute in self.blueprint.actions[req.action].params[param].model.validate ) {
+
+            //set attribute value to default if its not passed
+            if( Object.keys(parameters[param]).indexOf(attribute) === -1 ) {
+              parameters[param][attribute] = self.app.models[self.name].attributes[attribute].defaultsTo;
+            }
+
+            modelValidations = modelValidations.concat(self.app.services.validate.setup(self.blueprint.actions[req.action].params[param].model.validate[attribute], attribute, parameters[param][attribute], 'attribute'));
+
+          }
+
+        }
       }
 
       self.app.log.debug("performing " + transforms.length + " transforms, " + validations.length + " validations, and " + sanitizations.length + " sanitizations...");
@@ -81,6 +107,11 @@ module.exports = SuperJS.Class.extend({
         //.then(function() {
         //  return self.services.sanitize.process(sanitizations)
         //})
+
+        //execute validations
+        .then(function() {
+          return self.app.services.validate.process(modelValidations);
+        })
 
         //resolve if all passed without errors
         .then(function() {
@@ -102,6 +133,23 @@ module.exports = SuperJS.Class.extend({
 
   },
 
+  Describe: function(req) {
+
+    //maintain reference to instance
+    var self = this;
+
+    //return promise to resolve or reject
+    return new Promise(function(resolve, reject) {
+
+      //package response
+      var response = {meta: {success: true}, blueprint: self.blueprint};
+
+      //resolve response
+      resolve(response);
+
+    });
+
+  },
 
   //can be overridden by the controller extension to manipulate the request or response
   afterAction: function(req, response, next) {
