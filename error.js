001e7f4aebbc1c40bdb985a0159d148f432e3a8e
Aaron Storck <aaronstorck@gmail.com>
2015-01-22 21:28:20 -0500

    Updated the error class to capture all additional variables whether enumerable or not own property.

--- error.js
+++ error.js
@@ -16,16 +16,28 @@ module.exports = Error.extend({
 
   init: function(code, status, message, additional) {
 
+    //maintain reference to instance
+    var self = this;
+
     this.code = code;
     this.status = status;
     this.message = message;
 
     if( additional ) {
+
+      //copy all properties of this object and its prototype chain
       for( var property in additional ) {
         this[property] = additional[property];
       }
+
+      //make sure we get non-enumerable object properties
+      Object.getOwnPropertyNames(additional).map(function(property) {
+        self[property] = additional[property];
+      });
     }
 
-  }
+  },
+
+
 
 });
