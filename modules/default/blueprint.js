ed94ceec6c452b51ffb0b0ec54959d5e76d53196
Aaron Storck <aaronstorck@gmail.com>
2015-02-09 17:46:57 -0500

    Refactored the describe method to be more flexible.

--- modules/default/blueprint.js
+++ modules/default/blueprint.js
@@ -4,96 +4,31 @@
 
 module.exports = {
 
-  description: 'The default controller provides standard default methods for the root level of the API server.',
+  description: 'The default controller provides a basic set of methods for the API server.',
 
   actions: {
 
     default: {
-      description: "The default provides a basic response to allow you to determine whether the API is available."
+      description: "The default action returns the status of the API."
     },
 
     describe: {
 
-      description: "The describe method allows you to reflect the API's controllers & models.",
+      description: "The describe method reflects the available services, controllers, and models.",
       params: {
 
-        extensions: {
-          description: 'Choose whether to show blueprint extension paths.',
-          type: 'boolean',
-          default: false,
+        //todo: use options parameter object with merge transform
+        options: {
+          description: "An object expression which denotes what properties to return.",
+          type: "object",
+          default: {
+            controllers: true,
+            models: true
+          },
           transform: {
-            boolean: true
+            object: true
           }
-        },
-        controllers: {
-          description: 'Choose whether to return controllers.',
-          type: 'boolean',
-          default: true,
-          transform: {
-            boolean: true
-          }
-        },
-        actions: {
-          description: 'Choose whether to return actions.',
-          type: 'boolean',
-          default: false,
-          transform: {
-            boolean: true
-          }
-        },
-        params: {
-          description: 'Choose whether to return parameters.',
-          type: 'boolean',
-          default: false,
-          transform: {
-            boolean: true
-          }
-        },
-        models: {
-          description: 'Choose whether to return models.',
-          type: 'boolean',
-          default: true,
-          transform: {
-            boolean: true
-          }
-        },
-        attributes: {
-          description: 'Choose whether to return attributes.',
-          type: 'boolean',
-          default: false,
-          transform: {
-            boolean: true
-          }
-        },
-        transforms: {
-          description: 'Choose whether to return transforms.',
-          type: 'boolean',
-          default: false
-        },
-        validations: {
-          description: 'Choose whether to return validations.',
-          type: 'boolean',
-          default: false,
-          transform: {
-            boolean: true
-          }
-        },
-        sanitizations: {
-          description: 'Choose whether to return sanitizations.',
-          type: 'boolean',
-          default: false,
-          transform: {
-            boolean: true
-          }
-        },
-        descriptions: {
-          description: 'Choose whether to return descriptions.',
-          type: 'boolean',
-          default: true,
-          transform: {
-            boolean: true
-          }
-        },
+        }
 
       }
     }
--- modules/default/controller.js
+++ modules/default/controller.js
@@ -25,6 +25,102 @@ module.exports = SuperJS.Controller.extend({
 
   },
 
+  /**
+   * Describe the API using blueprints & models for reflection.
+   */
+
+  Describe: function(req) {
+
+    //maintain reference to instance
+    var self = this;
+
+    return new Promise(function(resolve, reject) {
+
+      //init response object
+      var response = { meta: { success: true } };
+
+      //localize the options parameter
+      var options = req.parameters.options;
+
+      //if controllers are enabled
+      if( options.controllers === true || typeof options.controllers === 'object' ) {
+
+        //create a controllers object for the response
+        response.controllers = {};
+
+        //loop through the loaded controllers
+        for ( var controller in self.app.controllers ) {
+
+          //copy the blueprint for the controller
+          response.controllers[controller] = JSON.parse(JSON.stringify(self.app.controllers[controller].blueprint));
+
+          if( typeof options.controllers === 'object' ) {
+            self.pruneObject(options.controllers, response.controllers[controller], controller);
+          }
+
+        }
+      }
+
+      //if models are enabled
+      if( options.models === true || typeof options.models === 'object' ) {
+
+        //create a controllers object for the response
+        response.models = {};
+
+        //loop through the loaded controllers
+        for ( var model in self.app.models ) {
+
+          console.log(self.app.models[model]);
+
+          //setup the model
+          response.models[model] = {};
+
+          //copy the model description
+          response.models[model].description = self.app.models[model].description;
+
+          //copy the model connection
+          response.models[model].connection = self.app.models[model].connection;
+
+          //copy the model attributes
+          response.models[model].attributes = JSON.parse(JSON.stringify(self.app.models[model].attributes));
+
+          if( typeof options.models === 'object' ) {
+            self.pruneObject(options.models, response.models[model], model);
+          }
+
+        }
+      }
+
+      resolve(response);
+
+    });
+  },
+
+  pruneObject: function(options,context,contextName) {
+
+    //loop through context properties and delete based on options
+    for( var property in context) {
+
+      //console.log(contextName, property, typeof options[property], typeof context, context);
+
+      //if the property is false or undefined on the options, delete it
+      if( typeof options[property] === 'undefined' || options[property] === false ) {
+
+        delete context[property];
+
+      } else if( typeof options[property] === 'object') {
+
+        for( var subProperty in context[property] ) {
+          this.pruneObject(options[property],context[property][subProperty],"." + contextName + "." + property + "." + subProperty);
+        }
+
+      }
+    }
+
+  }
+
+
+  /*
   //configure API describe response
   Describe: function(req) {
 
@@ -162,6 +258,8 @@ module.exports = SuperJS.Controller.extend({
     });
 
   },
+
+  */
 
 
 });
