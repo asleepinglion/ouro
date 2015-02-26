5b6dc656201308a18f36293ad7f227692a500187
Aaron Storck <aaronstorck@gmail.com>
2015-02-26 17:31:10 -0500

    Added parameter lookup method and cleaned code.

--- modules/base/controller.js
+++ modules/base/controller.js
@@ -63,38 +63,66 @@ module.exports = SuperJS.Class.extend({
         }
 
         //setup transforms
-        transforms = transforms.concat(self.app.services.transform.setup(self.blueprint.actions[req.action].params[param].transform, parameters, param));
+        transforms = transforms.concat(
+          self.app.services.transform.setup(
+            self.blueprint.actions[req.action].params[param].transform, parameters, param)
+        );
 
         //setup validations
-        validations = validations.concat(self.app.services.validate.setup(self.blueprint.actions[req.action].params[param].validate, parameters, param, 'parameter'));
+        validations = validations.concat(
+          self.app.services.validate.setup(
+            self.blueprint.actions[req.action].params[param].validate, parameters, param, 'parameter')
+        );
 
         //setup sanitizations
         //sanitizations = sanitizations.concat(self.app.services.sanitize.setup(self.blueprint.actions[req.action].params[param].sanitize, parameters, param));
 
+        //if the the parameter is a model, process attribtue transforms, validations, & sanitizations
         if( self.blueprint.actions[req.action].params[param].model ) {
 
-          //if the parameter is a model but the value is null/undefined, default parameter to empty object
-          if( Object.keys(self.blueprint.actions[req.action].params[param].model.validate).length > 0 && !parameters[param]) {
-            parameters[param] = {};
+          //if there are validations for this model and
+          if( Object.keys(self.blueprint.actions[req.action].params[param].model.validate).length > 0 &&
+
+            //the parameter has not been passed
+            typeof parameters[param] !== 'undefined') {
+
+              //loop through model validations
+              for (var attribute in self.blueprint.actions[req.action].params[param].model.validate) {
+
+                //if defaults are enabled for this action's model and
+                if( self.blueprint.actions[req.action].params[param].model.defaults === true &&
+
+                  //the attribute was not provided
+                  Object.keys(parameters[param]).indexOf(attribute) === -1) {
+
+                    //set the value of the attribute to it's specified default
+                    parameters[param][attribute] = self.app.models[self.name].attributes[attribute].defaultsTo;
+                }
+
+                //execute validations if the attribute is required or
+                if( self.blueprint.actions[req.action].params[param].model.validate[attribute].required === true ||
+
+                  //if the attribute has been provided
+                  typeof parameters[param][attribute] !== 'undefined') {
+
+                    modelValidations = modelValidations.concat(
+                      self.app.services.validate.setup(
+                        self.blueprint.actions[req.action].params[param].model.validate[attribute],
+                        parameters[param],
+                        attribute,
+                        'attribute'
+                      )
+                    );
+                }
+
+              }
+
           }
-
-          //loop through model validations
-          for( var attribute in self.blueprint.actions[req.action].params[param].model.validate ) {
-
-            //set attribute value to default if its not passed and required
-            if( Object.keys(parameters[param]).indexOf(attribute) === -1 && self.blueprint.actions[req.action].params[param].model.validate[attribute].required === true ) {
-              parameters[param][attribute] = self.app.models[self.name].attributes[attribute].defaultsTo;
-            }
-
-            //don't run validations if the attribute has not been provided and its not required
-            if( self.blueprint.actions[req.action].params[param].model.validate[attribute].required === true || typeof parameters[param][attribute] !== 'undefined' ) {
-              modelValidations = modelValidations.concat(self.app.services.validate.setup(self.blueprint.actions[req.action].params[param].model.validate[attribute], parameters[param], attribute, 'attribute'));
-            }
-
-          }
-
         }
       }
+
+      //self.app.log.debug('parameters:');
+      //self.app.log.object(parameters);
 
       var numberOfValidations = validations.length + modelValidations.length;
       self.app.log.debug("performing " + transforms.length + " transforms, " + numberOfValidations + " validations, and " + sanitizations.length + " sanitizations...");
@@ -134,6 +162,32 @@ module.exports = SuperJS.Class.extend({
         });
 
     });
+
+  },
+
+  //get a parameter's value after tranforms, validations, and sanitizations are complete
+  parameter: function(req, param) {
+
+    var context = undefined;
+
+    param = param.split('.');
+
+    for( var i = 0; i < param.length; i++ ) {
+
+      if( i === 0 ) {
+
+        if( typeof req.parameters !== 'object' || typeof req.parameters[param[0]] === 'undefined' ) {
+          break;
+        }
+
+        context = req.parameters[param[0]];
+      } else {
+        context = context[param[i]];
+      }
+
+    }
+
+    return context;
 
   },
 
